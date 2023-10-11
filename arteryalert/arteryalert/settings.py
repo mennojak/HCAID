6e391572af5abdeb99a92046d07c90084330c54b
Menno Jak <mennojak@gmail.com>
2023-10-11 13:58:14 +0200

    requirements.txt update + settings.py allowed host

--- arteryalert/arteryalert/settings.py
+++ arteryalert/arteryalert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(hxxrmljfb==)o6ppye(^@&ehdr$qa(_sk(lpozz-!9@+(n^@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
